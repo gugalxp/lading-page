72990db49ab13d9f2093ebfe536992e4c516efc9
Gustavo Arruda <gustavoleone3456@hotmail.com>
2024-01-30 19:42:04 -0300

    serviços

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,31 +1,13 @@
-import { Button, Carousel, Collapse, Image } from "antd";
+import { Button, Carousel, Image, List } from "antd";
+import { CheckOutlined, TrophyOutlined } from '@ant-design/icons';
 
 export function Home() {
 
-    const text = `
-  A dog is a type of domesticated animal.
-  Known for its loyalty and faithfulness,
-  it can be found as a welcome guest in many households across the world.
-`;
 
-    const items = [
-        {
-            key: '1',
-            label: 'This is panel header 1',
-            children: <p>{text}</p>,
-        },
-        {
-            key: '2',
-            label: 'This is panel header 2',
-            children: <p>{text}</p>,
-        },
-        {
-            key: '3',
-            label: 'This is panel header 3',
-            children: <p>{text}</p>,
-        },
+    const benefitsData = [
+        'Sinais Assertivos',
+        'Lives Operacionais que ensinam as pessoas a lerem a plataforma',
     ];
-
 
     return (
         <div>
@@ -48,11 +30,11 @@ export function Home() {
             <div className="parallax-container">
                 <div className="parallax-image slots" style={{ backgroundImage: `url('/image-background6.jpeg')` }}>
                     <div className="parallax-shadow"></div>
-                    <div className="parallax-content">
+                    <div className="parallax-content marginTopFive">
                         <div className="parallax-content-main">
                             <h1 style={{ color: 'white', fontSize: '30px' }} data-aos="fade-up" data-aos-duration="2000">
                                 Entre de cabeça na <span style={{ color: '#5662F6' }}>experiência frenética
-                                </span> dos SINAIS DE BRANCO, os mais emocionantes do 🌍
+                                </span> dos sinais de branco. O MAIOR DO MUNDO! 🌍
                             </h1>
                             <Button className="button" data-aos="zoom-in" data-aos-duration="2000">
                                 Eu quero lucrar todos os dias
@@ -61,11 +43,38 @@ export function Home() {
                     </div>
                 </div>
             </div>
+
+            <div className="parallax-container" style={{ height: '100vh' }}>
+                <div className="parallax-image roleta" style={{ backgroundImage: `url('/image-background5.jpeg')` }}>
+                    <div className="parallax-shadow"></div>
+                    <div className="title-carousel" data-aos="zoom-in-up" data-aos-duration="2000">
+                        <h1 style={{ color: '#5662F6', textAlign: 'center' }}>
+                            <span style={{ color: '#fff', marginRight: '.5em' }}>Benefícios</span><TrophyOutlined /></h1>
+                    </div>
+                    <div className="parallax-content">
+                        <div className="content-services">
+                            <List
+                                dataSource={benefitsData}
+                                renderItem={(item) => (
+                                    <List.Item style={{ color: '#fff', fontSize: '20px' }}>
+                                        <CheckOutlined style={{ color: '#5662F6', fontSize: '30px', fontWeight: 'bold' }} /> {item}
+                                    </List.Item>
+                                )}
+                            />
+
+                        <Button className="button" data-aos="zoom-in" data-aos-duration="2000">
+                            Eu quero lucrar todos os dias
+                        </Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <div className="title-carousel" data-aos="zoom-in-up" data-aos-duration="2000">
                 <h1 style={{ color: '#5662F6', textAlign: 'center' }}>Blaze <span style={{ color: '#fff' }}>pagando</span> 🔥</h1>
             </div>
             <div className="spacer" data-aos-duration="2000" data-aos="fade-up">
-                <Carousel slidesToShow={3} speed={2000} autoplay infinite={true} autoplaySpeed={1000} style={{ height: '100%' }}>
+                <Carousel slidesToShow={2} speed={2000} autoplay infinite={true} autoplaySpeed={1000} style={{ height: '100%' }}>
                     <Image.PreviewGroup>
                         <Image
                             src="/prova1.jpg"
@@ -119,18 +128,6 @@ export function Home() {
                         </Image.PreviewGroup>
                     </div>
                 </Carousel>
-            </div>
-
-            {/* Seção Roleta */}
-            <div className="parallax-container" style={{ height: '100vh' }}>
-                <div className="parallax-image roleta" style={{ backgroundImage: `url('/image-background5.jpeg')` }}>
-                    <div className="parallax-shadow"></div>
-                    <div className="parallax-content">
-                        <Collapse items={items} defaultActiveKey={true} />
-                        <Button className="button" data-aos="zoom-in" data-aos-duration="2000">
-                            Eu quero lucrar todos os dias
-                        </Button>                    </div>
-                </div>
             </div>
         </div>
     );
